27c4d19d7d56493a21c843ec1aded2f2a12b6da0
Tuomas J. Lukka <lukka@iki.fi>
1996-11-04 02:10:24 +0000

    Import of LUKKA/PDL-PP-0.003 from CPAN.
    gitpan-cpan-distribution: PDL-PP
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         LUKKA/PDL-PP-0.003.tar.gz
    gitpan-cpan-author:       LUKKA
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -16,7 +16,25 @@ print $b;
 # $c = pdl [0,0,0],[0,0,0],[0,0,0];
 $c = pdl [0,0],[0,0];
 
-PDLTEST5($a,$b,$c);
+PDLTEST2($a,$b,$c);
 
+$a = byte $a;
+$b = short $b;
+$c = byte pdl [0,0],[0,0];
+
+print "DT: $a->{Datatype}, $b->{Datatype} $c->{Datatype}\n";
+
+PDLTEST2($a,$b,$c);
+
+print $a; print $b;
 print $c;
+
+print "DT: $a->{Datatype}, $b->{Datatype} $c->{Datatype}\n";
+
+$a = pdl [0,0.1],[0.2,0.3];
+$b = pdl [0,0],[0,0];
+
+PDLTEST3($a,$b,0);
+
+print $a; print $b;
 
